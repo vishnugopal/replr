25e1178bf517baba21445a19a257f1cf1646a5b9
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2018-08-23 21:01:11 +0530

    Minor refactoring

--- lib/replr.rb
+++ lib/replr.rb
@@ -1,34 +1,49 @@
+require 'tmpdir'
+
+# :nodoc:
 class Replr
   attr_reader :arguments
 
   def start
     @arguments = ARGV.map { |argument| argument.downcase.strip }
 
-    unless arguments.length > 1
-      puts_error "Usage: replr <stack> <libraries...>"
-      exit
-    end
+    check_docker!
+    check_argument_length!
+    check_stack!
 
-    unless "ruby" == arguments[0]
-      puts_error "Only supports ruby for now"
-      exit
-    end
-
-    unless system("which docker >/dev/null")
-      puts_error "Needs docker installed & in path to work."
-      exit
-    end
-
-    extract_libraries
+    workdir = Dir.mktmpdir
+    Dir.chdir(workdir)
+    puts library_file_with(libraries)
   end
 
   private
-  def extract_libraries
-    libraries = arguments[1..-1]
-    puts gemfile_for_libraries(libraries)
+
+  def check_argument_length!
+    if arguments.empty?
+      puts_error 'Usage: replr <stack> <libraries...>'
+      exit
+    end
   end
 
-  def gemfile_for_libraries(libraries)
+  def check_stack!
+    unless arguments[0] == 'ruby'
+      puts_error 'Only supports ruby for now'
+      exit
+    end
+  end
+
+  def check_docker!
+    unless system('which docker >/dev/null')
+      puts_error 'Needs docker installed & in path to work.'
+      exit
+    end
+  end
+
+  def libraries
+    arguments[1..-1]
+  end
+
+  def library_file_with(libraries)
     gemfile = "source 'https://rubygems.org/'\n"
     libraries.each do |library|
       gemfile << "gem '#{library}'\n"
