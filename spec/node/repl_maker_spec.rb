b6057d0dc8f6d06f97dd4cb11720473363cde0d7
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2018-08-31 00:02:04 +0530

    Tweak documentation

--- spec/node/repl_maker_spec.rb
+++ spec/node/repl_maker_spec.rb
@@ -3,9 +3,20 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 require 'replr/process_runner'
+require 'replr/stack/node/repl_maker'
 
-describe Array do
-  # Just a shortcut to make life simpler
+describe Replr::Stack::Node::REPLMaker do
+  #
+  # This is a shortcut to `Replr::ProcessRunner#execute_repl_with_input`
+  # to make life easier.
+  #
+  # Note: we prefer integration tests to make sure all expected output is
+  # working correctly. This way, we can also make sure the REPL inside the
+  # docker container works well.
+  # Note how the inputs have the 'exit' command appended to the end to
+  # make sure we close the REPL stream correctly. Otherwise, we leave orphan
+  # docker processes lying around.
+  #
   def erwi(command, inputs, prompt_line, expected_output)
     Replr::ProcessRunner.new.execute_repl_with_input(command: "bin/replr #{command}",
                                                      inputs: inputs + ['.exit'],
