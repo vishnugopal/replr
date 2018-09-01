c184ed6311e0c14d386bd0f86898dbcb0ced45a7
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2018-09-01 08:51:24 +0530

    Refactor helpers into spec_helper

--- spec/node/repl_maker_spec.rb
+++ spec/node/repl_maker_spec.rb
@@ -5,24 +5,12 @@ require 'minitest/autorun'
 require 'replr/process_runner'
 require 'replr/stack/node/repl_maker'
 
+require_relative '../spec_helper'
+
 describe Replr::Stack::Node::REPLMaker do
-  #
-  # This is a shortcut to `Replr::ProcessRunner#execute_repl_with_input`
-  # to make life easier.
-  #
-  # Note: we prefer integration tests to make sure all expected output is
-  # working correctly. This way, we can also make sure the REPL inside the
-  # docker container works well.
-  # Note how the inputs have the 'exit' command appended to the end to
-  # make sure we close the REPL stream correctly. Otherwise, we leave orphan
-  # docker processes lying around.
-  #
-  def erwi(command, inputs, prompt_line, expected_output)
-    Replr::ProcessRunner.new.execute_repl_with_input(command: "bin/replr #{command}",
-                                                     inputs: inputs + ['.exit'],
-                                                     prompt_line: prompt_line,
-                                                     expected_output: expected_output)
-  end
+  include SpecHelper
+
+  before { configure_erwi(command_prefix: 'bin/replr', exit_line: '.exit') }
 
   it 'runs a simple node REPL' do
     erwi('node', ["console.log('1 + 2 =', 1 + 2)"],
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+module SpecHelper
+  def configure_erwi(command_prefix:, exit_line:)
+    @command_prefix = command_prefix
+    @exit_line = exit_line
+  end
+
+  #
+  # This is a shortcut to `Replr::ProcessRunner#execute_repl_with_input`
+  # to make life easier.
+  #
+  # Note: we prefer integration tests to make sure all expected output is
+  # working correctly. This way, we can also make sure the REPL inside the
+  # docker container works well.
+  # Note how the inputs have the 'exit' command appended to the end to
+  # make sure we close the REPL stream correctly. Otherwise, we leave orphan
+  # docker processes lying around.
+  #
+  def erwi(command, inputs, prompt_line, expected_output)
+    Replr::ProcessRunner.new.execute_repl_with_input(command: "#{@command_prefix} #{command}",
+                                                     inputs: inputs + [@exit_line],
+                                                     prompt_line: prompt_line,
+                                                     expected_output: expected_output)
+  end
+end
